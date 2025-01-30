b8a35b1c38b447f1574cbcc2d2acac67f1a95052
Stefani <stefanigtorres@gmail.com>
2025-01-30 10:12:23 -0500

    update

--- compression_tool/main.py
+++ compression_tool/main.py
@@ -1,9 +1,25 @@
-import argparse
 import pathlib
 
-parser = argparse.ArgumentParser()
-parser.add_argument("filepath", help="archivo a procesar", type=pathlib.Path)
-args = parser.parse_args()
+def char_counter(content: str):
+    frequency = {}
 
-with open(args.filepath, 'r') as reader:
-    reader.read()
+    for char in content:
+        frequency[char] = frequency.get(char, 0) + 1
+
+    return frequency
+
+
+def _cli():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", type=pathlib.Path)
+    args = parser.parse_args()
+
+    print(char_counter(args.file.read_text(encoding="utf-8")))
+
+if __name__ == "__main__":
+    _cli()
+
+
+
